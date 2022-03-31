ad6b58bbcb3ca92d093f2e36c5b3dcb20c78dde5
kuus <kunderikuus@gmail.com>
2022-03-31 15:57:45 +0200

    fix(deps): update

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,10 +1,10 @@
 export { Api, ApiConfig } from "./api.js";
 export { ApiGit, ApiGitConfig } from "./api-git.js";
 export { ApiGithub, ApiGithubConfig } from "./api-github.js";
-export { BaseConfig } from "./config-types";
+export type { BaseConfig } from "./config-types";
 export { Content, ContentConfig } from "./content.js";
 export { isTestEnv, encodePathname, normalisePathname } from "./helpers.js";
-export {
+export type {
   Kjam,
   EntriesMap,
   EntriesMapById,
